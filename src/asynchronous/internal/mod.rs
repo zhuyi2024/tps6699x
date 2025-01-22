df663f8719737ca482b9e0e3d8207f4ca90dc705
Robert Zieba <robertzieba@microsoft.com>
2025-01-22 13:32:33 -0700

    asynchronous/internal: Create `get_active_rdo_contract function` and tests

--- src/asynchronous/internal/mod.rs
+++ src/asynchronous/internal/mod.rs
@@ -141,6 +141,18 @@ impl<const N: usize, B: I2c> Tps6699x<N, B> {
             .read_async()
             .await
     }
+
+    /// Get active RDO contract
+    pub async fn get_active_rdo_contract(
+        &mut self,
+        port: PortId,
+    ) -> Result<registers::field_sets::ActiveRdoContract, Error<B::Error>> {
+        self.borrow_port(port)?
+            .into_registers()
+            .active_rdo_contract()
+            .read_async()
+            .await
+    }
 }
 
 #[cfg(test)]
@@ -354,5 +366,36 @@ mod test {
 
         test_get_active_pdo_contract(&mut tps6699x, PORT0, PORT0_ADDR1).await;
         test_get_active_pdo_contract(&mut tps6699x, PORT1, PORT1_ADDR1).await;
+    }
+
+    async fn test_get_active_rdo_contract(tps6699x: &mut Tps66994<Mock>, port: PortId, expected_addr: u8) {
+        use registers::field_sets::ActiveRdoContract;
+
+        let mut transactions = Vec::new();
+        // Read status register
+        transactions.push(create_register_read(expected_addr, 0x35, ActiveRdoContract::new_zero()));
+        tps6699x.bus.update_expectations(&transactions);
+
+        let active_rdo_contract = tps6699x.get_active_rdo_contract(port).await.unwrap();
+        assert_eq!(active_rdo_contract, ActiveRdoContract::new_zero());
+        tps6699x.bus.done();
+    }
+
+    #[tokio::test]
+    async fn test_get_active_rdo_contract_ports_0() {
+        let mock = Mock::new(&[]);
+        let mut tps6699x: Tps66994<Mock> = Tps6699x::new(mock, ADDR0);
+
+        test_get_active_rdo_contract(&mut tps6699x, PORT0, PORT0_ADDR0).await;
+        test_get_active_rdo_contract(&mut tps6699x, PORT1, PORT1_ADDR0).await;
+    }
+
+    #[tokio::test]
+    async fn test_get_active_rdo_contract_ports_1() {
+        let mock = Mock::new(&[]);
+        let mut tps6699x: Tps66994<Mock> = Tps6699x::new(mock, ADDR1);
+
+        test_get_active_rdo_contract(&mut tps6699x, PORT0, PORT0_ADDR1).await;
+        test_get_active_rdo_contract(&mut tps6699x, PORT1, PORT1_ADDR1).await;
     }
 }
