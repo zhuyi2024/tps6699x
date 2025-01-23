d40911024187f521d3c73d0909b022fd70199525
Robert Zieba <robertzieba@microsoft.com>
2025-01-23 11:59:00 -0700

    command: Add FW update command argument structs and timing constants

--- src/command.rs
+++ src/command.rs
@@ -1,6 +1,7 @@
+use bincode::de::Decoder;
 use bincode::enc::Encoder;
-use bincode::error::EncodeError;
-use bincode::Encode;
+use bincode::error::{DecodeError, EncodeError};
+use bincode::{Decode, Encode};
 use embedded_usb_pd::PdError;
 
 const CMD_LEN: usize = 4;
@@ -28,6 +29,14 @@ pub enum Command {
 
     /// Tomcat firmware update mode enter
     Tfus = u32_from_str("TFUs"),
+    /// Tomcat firmware update mode init
+    Tfui = u32_from_str("TFUi"),
+    /// Tomcat firmware update mode query
+    Tfuq = u32_from_str("TFUq"),
+    /// Tomcat firmware update mode exit
+    Tfue = u32_from_str("TFUe"),
+    /// Tomcat firmware update data
+    Tfud = u32_from_str("TFUd"),
     /// Tomcat firmware update complete
     Tfuc = u32_from_str("TFUc"),
 }
@@ -39,6 +48,9 @@ impl Command {
             // Reset-type commands don't need to be checked
             Command::Success | Command::Invalid | Command::Gaid | Command::Tfus => 0,
             Command::Tfuc => 5000,
+            Command::Tfui => 1500,
+            Command::Tfuq | Command::Tfue => 200,
+            Command::Tfud => 7000,
         }
     }
 }
@@ -142,6 +154,208 @@ impl Encode for ResetArgs {
 
 /// Delay for completion of TFUs command
 pub(crate) const TFUS_DELAY_MS: u32 = 500;
+/// Timeout for completion of TFUs command
+#[allow(dead_code)]
+pub(crate) const TFUS_TIMEOUT_MS: u32 = TFUS_DELAY_MS + 100;
+#[allow(dead_code)]
+pub(crate) const TFUI_TIMEOUT_MS: u32 = 100;
+#[allow(dead_code)]
+pub(crate) const TFUE_TIMEOUT_MS: u32 = 100;
+#[allow(dead_code)]
+pub(crate) const TFUD_TIMEOUT_MS: u32 = 100;
+#[allow(dead_code)]
+pub(crate) const TFUQ_TIMEOUT_MS: u32 = 100;
+#[allow(dead_code)]
+pub(crate) const RESET_TIMEOUT_MS: u32 = RESET_DELAY_MS + 100;
+
+#[allow(dead_code)]
+pub(crate) const TFUI_ARGS_LEN: usize = 8;
+
+#[derive(Debug, Clone, Copy, Decode, Encode, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub struct TfuiArgs {
+    pub num_data_blocks_tx: u16,
+    pub data_len: u16,
+    pub timeout_secs: u16,
+    pub broadcast_u16_address: u16,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+#[repr(u8)]
+pub enum TfuqCommandType {
+    QueryTfuStatus = 0x00,
+}
+
+impl Encode for TfuqCommandType {
+    fn encode<E: Encoder>(&self, encoder: &mut E) -> Result<(), EncodeError> {
+        let val = *self as u8;
+        Encode::encode(&val, encoder)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+#[repr(u8)]
+pub enum TfuqStatusQuery {
+    StatusDefault = 0x00,
+    StatusInProgress,
+    StatusBank0,
+    StatusBank1,
+}
+
+impl Encode for TfuqStatusQuery {
+    fn encode<E: Encoder>(&self, encoder: &mut E) -> Result<(), EncodeError> {
+        let val = *self as u8;
+        Encode::encode(&val, encoder)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+#[repr(u8)]
+pub enum TfuqBlockStatus {
+    Success = 0x0,
+    InvalidTfuState,
+    InvalidHeaderSize,
+    InvalidDataBlock,
+    InvalidDataSize,
+    InvalidSlaveAddress,
+    InvalidTimeout,
+    MaxAppConfigUpdate,
+    HeaderRxInProgress,
+    HeaderValidAndAuthentic,
+    HeaderNotValid,
+    HeaderKeyNotValid,
+    HeaderRootAuthFailure,
+    HeaderFwheaderAuthFailure,
+    DataRxInProgress,
+    DataValidAndAuthentic,
+    DataValidButRepeated,
+    DataNotValid,
+    DataInvalidId,
+    DataAuthFailure,
+    F911IdNotValid,
+    F911DataNotValid,
+    F911AuthFailure,
+    ImageDownloadTimeout,
+    BlockDownloadTimeout,
+    BlockWriteFailed,
+    SpecialCmdFailed,
+}
+
+impl TryFrom<u8> for TfuqBlockStatus {
+    type Error = PdError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0x0 => Ok(TfuqBlockStatus::Success),
+            0x1 => Ok(TfuqBlockStatus::InvalidTfuState),
+            0x2 => Ok(TfuqBlockStatus::InvalidHeaderSize),
+            0x3 => Ok(TfuqBlockStatus::InvalidDataBlock),
+            0x4 => Ok(TfuqBlockStatus::InvalidDataSize),
+            0x5 => Ok(TfuqBlockStatus::InvalidSlaveAddress),
+            0x6 => Ok(TfuqBlockStatus::InvalidTimeout),
+            0x7 => Ok(TfuqBlockStatus::MaxAppConfigUpdate),
+            0x8 => Ok(TfuqBlockStatus::HeaderRxInProgress),
+            0x9 => Ok(TfuqBlockStatus::HeaderValidAndAuthentic),
+            0xA => Ok(TfuqBlockStatus::HeaderNotValid),
+            0xB => Ok(TfuqBlockStatus::HeaderKeyNotValid),
+            0xC => Ok(TfuqBlockStatus::HeaderRootAuthFailure),
+            0xD => Ok(TfuqBlockStatus::HeaderFwheaderAuthFailure),
+            0xE => Ok(TfuqBlockStatus::DataRxInProgress),
+            0xF => Ok(TfuqBlockStatus::DataValidAndAuthentic),
+            0x10 => Ok(TfuqBlockStatus::DataValidButRepeated),
+            0x11 => Ok(TfuqBlockStatus::DataNotValid),
+            0x12 => Ok(TfuqBlockStatus::DataInvalidId),
+            0x13 => Ok(TfuqBlockStatus::DataAuthFailure),
+            0x14 => Ok(TfuqBlockStatus::F911IdNotValid),
+            0x15 => Ok(TfuqBlockStatus::F911DataNotValid),
+            0x16 => Ok(TfuqBlockStatus::F911AuthFailure),
+            0x17 => Ok(TfuqBlockStatus::ImageDownloadTimeout),
+            0x18 => Ok(TfuqBlockStatus::BlockDownloadTimeout),
+            0x19 => Ok(TfuqBlockStatus::BlockWriteFailed),
+            0x1A => Ok(TfuqBlockStatus::SpecialCmdFailed),
+            _ => Err(PdError::InvalidParams),
+        }
+    }
+}
+
+impl Decode for TfuqBlockStatus {
+    fn decode<D: Decoder>(decoder: &mut D) -> Result<Self, DecodeError> {
+        let val: u8 = Decode::decode(decoder)?;
+        TfuqBlockStatus::try_from(val).map_err(|_| DecodeError::Other("Invalid TfuqBlockStatus"))
+    }
+}
+
+#[allow(dead_code)]
+pub(crate) const TFUQ_ARGS_LEN: usize = 2;
+
+#[derive(Debug, Encode, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub struct TfuqArgs {
+    pub status_query: TfuqStatusQuery,
+    pub command: TfuqCommandType,
+}
+
+#[allow(dead_code)]
+pub(crate) const TFUQ_RETURN_LEN: usize = 40;
+#[allow(dead_code)]
+pub(crate) const TFUQ_RETURN_BLOCK_STATUS_LEN: usize = 13;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub struct TfuqReturnValue {
+    pub active_host: u8,
+    pub current_state: u8,
+    pub image_write_status: u8,
+    pub blocks_written_bitfield: u16,
+    pub block_status: [TfuqBlockStatus; TFUQ_RETURN_BLOCK_STATUS_LEN],
+    pub num_of_header_bytes_received: u32,
+    pub num_of_data_bytes_received: u32,
+    pub num_of_app_config_updates: u16,
+}
+
+impl Decode for TfuqReturnValue {
+    fn decode<D: Decoder>(decoder: &mut D) -> Result<Self, DecodeError> {
+        let active_host = Decode::decode(decoder)?;
+        let current_state = Decode::decode(decoder)?;
+        let _reserved: u16 = Decode::decode(decoder)?;
+        let image_write_status = Decode::decode(decoder)?;
+        let blocks_written_bitfield = Decode::decode(decoder)?;
+        let mut block_status = [TfuqBlockStatus::Success; TFUQ_RETURN_BLOCK_STATUS_LEN];
+        for status in block_status.iter_mut() {
+            *status = Decode::decode(decoder)?;
+        }
+        let num_of_header_bytes_received = Decode::decode(decoder)?;
+        let _reserved: u16 = Decode::decode(decoder)?;
+        let num_of_data_bytes_received = Decode::decode(decoder)?;
+        let _reserved: u16 = Decode::decode(decoder)?;
+        let num_of_app_config_updates = Decode::decode(decoder)?;
+
+        Ok(TfuqReturnValue {
+            active_host,
+            current_state,
+            image_write_status,
+            blocks_written_bitfield,
+            block_status,
+            num_of_header_bytes_received,
+            num_of_data_bytes_received,
+            num_of_app_config_updates,
+        })
+    }
+}
+
+#[allow(dead_code)]
+pub(crate) const TFUD_ARGS_LEN: usize = 8;
+#[derive(Debug, Decode, Encode, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub struct TfudArgs {
+    pub block_number: u16,
+    pub data_len: u16,
+    pub timeout_secs: u16,
+    pub broadcast_u16_address: u16,
+}
 
 #[cfg(test)]
 mod test {
@@ -179,5 +393,93 @@ mod test {
             },
             [0xAC, 0xAC],
         );
+    }
+
+    #[test]
+    fn test_tfui_args_encode_decode() {
+        let args = TfuiArgs {
+            num_data_blocks_tx: 0x1234,
+            data_len: 0x5678,
+            timeout_secs: 0x9ABC,
+            broadcast_u16_address: 0xDEF0,
+        };
+        let expected = [0x34, 0x12, 0x78, 0x56, 0xBC, 0x9A, 0xF0, 0xDE];
+        let mut buf = [0; TFUI_ARGS_LEN];
+
+        // Test encoding
+        bincode::encode_into_slice(args, &mut buf, config::standard().with_fixed_int_encoding()).unwrap();
+        assert_eq!(buf, expected);
+
+        // Test decoding
+        let (decoded, _): (TfuiArgs, _) =
+            bincode::decode_from_slice(&buf, config::standard().with_fixed_int_encoding()).unwrap();
+        assert_eq!(decoded, args);
+    }
+
+    #[test]
+    fn test_tfuq_args_encode() {
+        let args = TfuqArgs {
+            status_query: TfuqStatusQuery::StatusBank0,
+            command: TfuqCommandType::QueryTfuStatus,
+        };
+        let mut buf = [0; TFUQ_ARGS_LEN];
+        bincode::encode_into_slice(args, &mut buf, config::standard().with_fixed_int_encoding()).unwrap();
+        assert_eq!(buf, [0x02, 0x00]);
+    }
+
+    #[test]
+    fn test_tfuq_return_value_decode() {
+        let args = TfuqReturnValue {
+            active_host: 0x01,
+            current_state: 0x02,
+            image_write_status: 0x03,
+            blocks_written_bitfield: 0x0405,
+            block_status: [
+                TfuqBlockStatus::Success,
+                TfuqBlockStatus::InvalidTfuState,
+                TfuqBlockStatus::InvalidHeaderSize,
+                TfuqBlockStatus::InvalidDataBlock,
+                TfuqBlockStatus::InvalidDataSize,
+                TfuqBlockStatus::InvalidSlaveAddress,
+                TfuqBlockStatus::InvalidTimeout,
+                TfuqBlockStatus::MaxAppConfigUpdate,
+                TfuqBlockStatus::HeaderRxInProgress,
+                TfuqBlockStatus::HeaderValidAndAuthentic,
+                TfuqBlockStatus::HeaderNotValid,
+                TfuqBlockStatus::HeaderKeyNotValid,
+                TfuqBlockStatus::HeaderRootAuthFailure,
+            ],
+            num_of_header_bytes_received: 0x06070809,
+            num_of_data_bytes_received: 0x0A0B0C0D,
+            num_of_app_config_updates: 0x0E0F,
+        };
+        let expected = [
+            0x01, 0x02, 0x00, 0x00, 0x03, 0x05, 0x04, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A,
+            0x0B, 0xC, 0x09, 0x08, 0x07, 0x06, 0x00, 0x00, 0x0D, 0x0C, 0x0B, 0x0A, 0x00, 0x00, 0x0F, 0x0E,
+        ];
+        let (decoded, _): (TfuqReturnValue, _) =
+            bincode::decode_from_slice(&expected, config::standard().with_fixed_int_encoding()).unwrap();
+        assert_eq!(decoded, args);
+    }
+
+    #[test]
+    fn test_tfud_args_encode_decode() {
+        let args = TfudArgs {
+            block_number: 0x1234,
+            data_len: 0x5678,
+            timeout_secs: 0x9ABC,
+            broadcast_u16_address: 0xDEF0,
+        };
+        let expected = [0x34, 0x12, 0x78, 0x56, 0xBC, 0x9A, 0xF0, 0xDE];
+        let mut buf = [0; TFUD_ARGS_LEN];
+
+        // Test encoding
+        bincode::encode_into_slice(args, &mut buf, config::standard().with_fixed_int_encoding()).unwrap();
+        assert_eq!(buf, expected);
+
+        // Test decoding
+        let (decoded, _): (TfudArgs, _) =
+            bincode::decode_from_slice(&buf, config::standard().with_fixed_int_encoding()).unwrap();
+        assert_eq!(decoded, args);
     }
 }
